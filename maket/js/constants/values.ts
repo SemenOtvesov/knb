a4c50724ddcab0f02489b583ada7999c8b5fa515
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2025-02-21 21:06:42 +0300

    d

--- maket/js/constants/values.ts
+++ maket/js/constants/values.ts
@@ -3,3 +3,5 @@ export const baseUrl = 'http://localhost:3000';
 export const inviteUrl = 'http://t.me/spor_game_bot/spors?startapp=';
 
 export const APP_URL = 'https://knb-inky.vercel.app';
+export const RAW_MANIFEST =
+    'https://raw.githubusercontent.com/SemenOtvesov/knb/refs/heads/main/tonconnect-manifest.json';
--- maket/js/script.js
+++ maket/js/script.js
@@ -5,14 +5,14 @@ import App from '@js/components/mainComponents/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '@state/store';
-import { APP_URL } from './constants/values';
+import { APP_URL, RAW_MANIFEST } from './constants/values';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 const wrap = document.querySelector('#wrap');
 
 const root = createRoot(wrap);
 root.render(
-    <TonConnectUIProvider manifestUrl={`https://${APP_URL}/tonconnect-manifest.json`}>
+    <TonConnectUIProvider manifestUrl={`https://${RAW_MANIFEST}/tonconnect-manifest.json`}>
         <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
             <Provider store={store}>
                 <App />
